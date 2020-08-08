36e49516fe162202d068e9ce69fe41b8f80ad2c8
Geart Ferhati <55233184+geartprogrammer@users.noreply.github.com>
2020-08-08 20:20:46 +0200

    Update Email.py

--- online_identity_generator/online_identity_generator/online_identity_generator/Email.py
+++ online_identity_generator/online_identity_generator/online_identity_generator/Email.py
@@ -2,4 +2,10 @@ from .UserItem import UserItem
 
 class Email(UserItem):
     def generate(self):
-        pass
+        from random import choice, randint
+        from string import ascii_letters
+
+        if self.topic.lower() == 'casual' or self.topic.lower == '':
+            alphabet = list(ascii_letters)
+            email1 = [f'{self.include_words}{randint(0,1000)}' for i in range(5)]
+            return email1
